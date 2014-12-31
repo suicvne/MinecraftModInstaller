5d79039faedd65707ec5aa93d38b4e618bbad2a3
Mike Santiago <miketheripper1@gmail.com>
2014-12-31 10:22:30 -0500

    finished scripts

--- scripts/linux/linux_installer.sh
+++ scripts/linux/linux_installer.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 DESKTOPLAUNCHERURL="https://raw.githubusercontent.com/Luigifan/MinecraftModInstaller/master/scripts/linux/Minecraft%20Mod%20Installer.desktop"
 LAUNCHSCRIPTURL="https://raw.githubusercontent.com/Luigifan/MinecraftModInstaller/master/scripts/linux/launch.sh"
 LATESTJARURL="https://github.com/Luigifan/MinecraftModInstaller/blob/master/scripts/Minecraft%20Mod%20Installer.app/Contents/MacOS/mcmodinstaller.jar?raw=true"
 ICONURL="https://raw.githubusercontent.com/Luigifan/MinecraftModInstaller/master/src/res/icon.png"
+
 
 #1. Check if the dir ~/.mcmodinstaller exists
 #2. Make if necessary
@@ -12,38 +13,28 @@ ICONURL="https://raw.githubusercontent.com/Luigifan/MinecraftModInstaller/master
 
 echo "Making necessary directories.."
 
-if [ ! -d "~/.mcmodinstaller" ]; then
-	mkdir "~/.mcmodinstaller"
+if [ ! -d "$HOME/.mcmodinstaller" ]; then
+	mkdir "$HOME/.mcmodinstaller"
 fi
 
 echo "Checking for Java.."
 #check if java even exists
-if type -p java; then
-
-elif [[ -n "$JAVA_HOME"]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-
-else
-	echo "no java"
-	exit
-fi
-
-echo "Checking Java version.."
-#check version
-if [["$_java"]]; then
-	version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-	echo version "$version"
-	if [[ "$version" > "1.6"]]; then
-		echo "good"
-	else
-		echo "you need at least Java 1.7 to run this program!"
-		exit
-	fi
-fi
-
 echo "Retrieving files.."
 #download
-wget -O "~/.mcmodinstaller/mcmodinstaller.jar" $LATESTJARURL
-wget -O "~/.local/share/applications/Minecraft Mod Installer.desktop" $DESKTOPLAUNCHERURL
-wget -O "~/.mcmodinstaller/icon.png" $ICONURL
-wget -O "~/.mcmodinstaller/launch.sh" $LAUNCHSCRIPTURL
+wget -q -O "$HOME/.mcmodinstaller/mcmodinstaller.jar" $LATESTJARURL
+wget -q -O "$HOME/.local/share/applications/Minecraft Mod Installer.desktop" $DESKTOPLAUNCHERURL
+wget -q -O "$HOME/.mcmodinstaller/icon.png" $ICONURL
+wget -q -O "$HOME/.mcmodinstaller/launch.sh" $LAUNCHSCRIPTURL
+
+echo "Writing .desktop.."
+#
+echo -e "#!/usr/bin/env xdg-open\n[Desktop Entry]\nVersion=1.0\nType=Application\nTerminal=false\nName=Minecraft Mod Installer\nComment=\"A simple Java based mod installer for Minecraft\"\nExec=java -jar $HOME/.mcmodinstaller/mcmodinstaller.jar\nIcon=$HOME/.mcmodinstaller/icon.png" >> "$HOME/.local/share/applications/mcmodinstaller.desktop"
+#
+echo "Setting permissions.."
+chmod +x "$HOME/.mcmodinstaller/launch.sh"
+chmod +x "$HOME/.local/share/applications/mcmodinstaller.desktop"
+#
+echo "All files seem to be installed correctly!"
+echo "Press enter to exit.."
+read -n1
 
